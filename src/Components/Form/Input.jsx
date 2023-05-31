7bfd512db2031c761fb58d62628303f06a19ee9d
MAwasTaken <ce.mahdiabdollahi@gmail.com>
2023-05-31 13:33:36 +0330

    Input Reducer Created

--- src/Components/Form/Input.jsx
+++ src/Components/Form/Input.jsx
@@ -1,5 +1,5 @@
 // react
-import React from 'react';
+import React, { useReducer } from 'react';
 
 // styles
 import './Input.css';
@@ -10,17 +10,48 @@ import './Input.css';
 
 // input
 function Input(props) {
+	// change handler
+	const inputReducer = (state, action) => {
+		switch (action.type) {
+			case 'CHANGE': {
+				return {
+					...state,
+					value: action.value,
+					isValid: true,
+				};
+			}
+			default: {
+				return state;
+			}
+		}
+	};
+
+	const [mainInput, dispatch] = useReducer(inputReducer, {
+		value: '',
+		isValid: false,
+	});
+
+	const onChangeHandler = (event) =>
+		dispatch({ type: 'CHANGE', value: event.target.value, isValid: false });
+
+	// input element creator
 	const element =
 		props.element === 'input' ? (
 			<input
-				className={props.className}
+				className={`${props.className} ${
+					mainInput.value === '' ? `null` : mainInput.isValid ? 'success' : 'error'
+				}`}
 				type={props.type}
 				placeholder={props.placeholder}
+				onChange={onChangeHandler}
+				value={mainInput.value}
 			/>
 		) : (
 			<textarea
 				className={props.className}
 				placeholder={props.placeholder}
+				onChange={onChangeHandler}
+				value={mainInput.value}
 			/>
 		);
 	// jsx
